791ab377e6d6fd01d06dbe9817e0624cd485b5f3
Mikael Lind <elemel@elemel.se>
2021-10-24 12:26:56 +0200

    Extract Pong example

--- examples/pong/main.lua
+++ examples/pong/main.lua
@@ -2,7 +2,7 @@ local Game = require("heartable.Game")
 local Struct = require("heartable.StructType")
 
 function love.load()
-  love.window.setTitle("Heartable")
+  love.window.setTitle("Pong")
   love.physics.setMeter(1)
 
   game = Game.new()
